fb03f2de2cb6ac71ba0d2f3085ec0d0d90aaa95c
unknown <ferdiansyahsaptraa@gmail.com>
2025-09-09 21:57:42 +0700

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>arba</h1>
+      <h1 className='text-blue-700'>arba</h1>
     </div>
     // <>
     //   <div>
